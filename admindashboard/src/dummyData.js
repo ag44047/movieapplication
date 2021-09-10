f582a5a9c4bdd92876873472fa4984abf3d57886
RinaKorca <73649582+RinaKorca@users.noreply.github.com>
2021-09-10 12:23:27 +0200

    admin dashboard almost finished

---
+++ admindashboard/src/dummyData.js
@@ -0,0 +1,214 @@
+export const userData = [
+    {
+        name: "Jan",
+        "Active User": 4000,
+    },
+    {
+        name: "Feb",
+        "Active User": 3000,
+    },
+    {
+        name: "Mar",
+        "Active User": 5000,
+    },
+    {
+        name: "Apr",
+        "Active User": 4000,
+    },
+    {
+        name: "May",
+        "Active User": 3000,
+    },
+    {
+        name: "Jun",
+        "Active User": 2000,
+    },
+    {
+        name: "Jul",
+        "Active User": 4000,
+    },
+    {
+        name: "Agu",
+        "Active User": 3000,
+    },
+    {
+        name: "Sep",
+        "Active User": 4000,
+    },
+    {
+        name: "Oct",
+        "Active User": 1000,
+    },
+    {
+        name: "Nov",
+        "Active User": 4000,
+    },
+    {
+        name: "Dec",
+        "Active User": 3000,
+    },
+];
+
+
+export const userRows = [
+    {
+        id: 1, username: 'Rina Korca', avatar: "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        email: "rinakorca2001@gmail.com",
+        status: "active",
+        transaction: "$120.00",
+    },
+    {
+        id: 2, username: 'Rina Korca', avatar: "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        email: "rinakorca2001@gmail.com",
+        status: "active",
+        transaction: "$120.00",
+    },
+    {
+        id: 3, username: 'Rina Korca', avatar: "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        email: "rinakorca2001@gmail.com",
+        status: "active",
+        transaction: "$120.00",
+    },
+    {
+        id: 4, username: 'Rina Korca', avatar: "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        email: "rinakorca2001@gmail.com",
+        status: "active",
+        transaction: "$120.00",
+    },
+    {
+        id: 5, username: 'Rina Korca', avatar: "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        email: "rinakorca2001@gmail.com",
+        status: "active",
+        transaction: "$120.00",
+    },
+    {
+        id: 6, username: 'Rina Korca', avatar: "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        email: "rinakorca2001@gmail.com",
+        status: "active",
+        transaction: "$120.00",
+    },
+    {
+        id: 7, username: 'Rina Korca', avatar: "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        email: "rinakorca2001@gmail.com",
+        status: "active",
+        transaction: "$120.00",
+    },
+    {
+        id: 8, username: 'Rina Korca', avatar: "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        email: "rinakorca2001@gmail.com",
+        status: "active",
+        transaction: "$120.00",
+    },
+    {
+        id: 9, username: 'Rina Korca', avatar: "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        email: "rinakorca2001@gmail.com",
+        status: "active",
+        transaction: "$120.00",
+    },
+];
+export const productRows = [
+    {
+        id: 1,
+        name: "The Matrix",
+        img: "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        stock: 123,
+        status: "active",
+        price: "$120.00",
+    },
+    {
+        id: 2,
+        name: "The Matrix",
+        img:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        stock: 123,
+        status: "active",
+        price: "$120.00",
+    },
+    {
+        id: 3,
+        name: "The Matrix",
+        img:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        stock: 123,
+        status: "active",
+        price: "$120.00",
+    },
+    {
+        id: 4,
+        name: "The Matrix",
+        img:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        stock: 123,
+        status: "active",
+        price: "$120.00",
+    },
+    {
+        id: 5,
+        name: "The Matrix",
+        img:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        stock: 123,
+        status: "active",
+        price: "$120.00",
+    },
+    {
+        id: 6,
+        name: "The Matrix",
+        img:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        stock: 123,
+        status: "active",
+        price: "$120.00",
+    },
+    {
+        id: 7,
+        name: "The Matrix",
+        img:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        stock: 123,
+        status: "active",
+        price: "$120.00",
+    },
+    {
+        id: 8,
+        name: "The Matrix",
+        img:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        stock: 123,
+        status: "active",
+        price: "$120.00",
+    },
+    {
+        id: 9,
+        name: "The Matrix",
+        img:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        stock: 123,
+        status: "active",
+        price: "$120.00",
+    },
+    {
+        id: 10,
+        name: "The Matrix",
+        img:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        stock: 123,
+        status: "active",
+        price: "$120.00",
+    },
+];
+export const productData = [
+    {
+        name: "Jan",
+        "Sales": 4000,
+    },
+    {
+        name: "Feb",
+        "Sales": 3000,
+    },
+    {
+        name: "Mar",
+        "Sales": 5000,
+    },
+];
+
