c31a2753c840d5878b933f86f4b2b09be7a64f43
RinaKorca <73649582+RinaKorca@users.noreply.github.com>
2021-09-18 11:29:44 +0200

    movie crud completed

---
+++ Application/Movies/Details.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Movies
+{
+    public class Details
+    {
+        public class Query : IRequest<Movie>
+        {
+            public Guid Id { get; set; }
+
+        }
+        public class Handler : IRequestHandler<Query, Movie>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Movie> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Movies.FindAsync(request.Id);
+            }
+        }
+    }
+}
---
+++ Application/Movies/MovieList.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Movies
+{
+    public class MovieList
+    {
+        public class Query : IRequest<List<Movie>>
+        {
+
+        }
+        public class Handler : IRequestHandler<Query, List<Movie>>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Movie>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Movies.ToListAsync();
+            }
+        }
+    }
+}
