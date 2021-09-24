c4e46d571c05192025c5ccc516207431b272acf4
Doruntina Korca <dk40651@ubt-uni.net>
2021-09-24 19:18:55 +0200

    User CRUD and user Roles added

---
+++ -- SQLite.sql
@@ -0,0 +1,3 @@
+-- SQLite
+INSERT INTO AspNetUserRoles (UserId, RoleId)
+VALUES ("0788b7ec-b7c1-4908-bdde-79d7925d2d61","f765276b-baf2-4f44-88eb-5b95eea43761");
---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class UserDto
+    {
+
+        public string DisplayName { get; set; }
+
+        public string Token { get; set; }
+
+        public string Username { get; set; }
+
+        public string UserImage { get; set; }
+    }
+}
